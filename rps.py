d1c7fd8af7fe797492d881ddc5bd9746ac8ac5e6
Irene D. Martinez <imartinez921@gmail.com>
2023-11-30 12:41:54 -0800

    Ch 5 - User Inputs - Create rock-paper-scissors game

--- rps.py
+++ rps.py
@@ -1,2 +1,26 @@
-value = input("Please enter a value")
-print(value)
+import sys
+import random
+
+player_input = input("Please enter a value:\n1 for Rock\n2 for Paper\n3 for "
+                     "Scissors\n\nYour input: ")
+
+player_choice = int(player_input)
+
+if player_choice <1 or player_choice >3:
+    sys.exit("Invalid choice. Please try again.") # Will exit the control flow bc error
+
+computer_input = random.choice("123")
+computer_choice = int(computer_input)
+print(computer_choice)
+
+print("")
+print("You chose " + player_input+ ".")
+print("Python chose " + computer_input + ".")
+print("")
+
+if player_choice > computer_choice:
+    print("You won!!!")
+elif computer_choice > player_choice:
+    print("You lost! Python won.")
+else:
+    print("It's a tie! Play again.")
